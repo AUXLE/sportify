1b3a753fad0884e25c7eee1cf7f9c11a197d2292
Jaya Prakash K <auxle2022@gmail.com>
2024-12-04 09:56:50 +0000

    Solved the font issue

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sportify/screens/create_account.dart';
 import 'package:sportify/screens/tab.dart';
